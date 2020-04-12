dc3fd406773aefb3a8bddf3bbac954d3b89d1036
cvary61 <61945481+cvary61@users.noreply.github.com>
2020-04-12 22:09:50 +0300

    Update login.lua

--- login.lua
+++ login.lua
@@ -4,7 +4,7 @@ if not PS then os.exit()
 end 
 if PS[1] == Password then 
 gg.alert('✔️ Şifre Doğrulandı❕','Devam ↪️')
-V = load(gg.makeRequest("https://github.com/cvary61/luascript/raw/master/script2.lua").content)
+V = load(gg.makeRequest("https://github.com/cvary61/luascript/raw/master/main.lua").content)
 pcall(V)
 else 
 gg.alert('❌ Şifre Yanlış ❕') os.exit() end
